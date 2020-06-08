c75b9078ef55172a2b7d0ee63f5e827c86288157
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-06-08 17:41:40 +0200

    We modify the TicketCreator.vue component

--- src/components/TicketCreator.vue
+++ src/components/TicketCreator.vue
@@ -1,34 +1,32 @@
 <template>
   <q-dialog v-model="show" @before-hide="cleanTicket">
     <q-card style="min-width: 550px; ">
-      <q-card-section class="q-pa-sm">
+      <q-card-section class="q-pa-md">
         <div class="text-h6">Create new ticket</div>
       </q-card-section>
       <q-separator inset=""/>
 
-      <q-card-section class="q-pa-none row">
-        <div class="col-12 col-md-6 q-pa-sm">
+      <q-card-section class="q-pa-md row">
+        <div class="col-12 col-md-6 q-pr-sm">
           <q-input outlined dense v-model="ticket.title" autofocus placeholder="Title" class="q-mb-md"/>
-          <q-editor v-model="ticket.description" min-height="5rem" placeholder="Here goes the ticket description...."
+          <q-editor v-model="ticket.description" min-height="5rem" placeholder="Here goes the ticket description..."
                     :toolbar="[['bold', 'italic', 'strike', 'underline']]"/>
         </div>
-        <div class="col-12 col-md-6 q-pa-sm">
-          <q-select dense outlined v-model="selectedCategory" :options="categories" value="code" label="Category"
+        <div class="col-12 col-md-6 q-pl-sm">
+          <q-select dense outlined v-model="selectedCategory" class="q-pb-xs" :options="categories" value="code"
+                    label="Category"
                     @input="ticket.idCategory = selectedCategory.code"/>
-          <div v-if="selectedCategory!=null" class="q-pl-md q-my-md">
-            {{selectedCategory.description}}
-          </div>
+          <div v-if="selectedCategory!=null" class="q-pa-xs q-my-xs" v-html="selectedCategory.description"></div>
           <q-select dense outlined v-model="selectedStatus" :options="statuses" value="code" label="Status"
                     @input="ticket.idStatus = selectedStatus.code"/>
-          <div v-if="selectedStatus!=null" class="q-pl-md q-my-md">
-            {{selectedStatus.description}}
-          </div>
+          <div v-if="selectedStatus!=null" class="q-pa-xs q-my-xs" v-html="selectedStatus.description"></div>
         </div>
 
 
       </q-card-section>
+      <q-separator inset=""/>
 
-      <q-card-actions align="right" class="text-primary">
+      <q-card-actions align="right" class="text-primary q-pr-sm">
         <q-btn flat label="Cancel" color="red-10" v-close-popup/>
         <q-btn flat label="Save ticket" @click="addTicket"/>
       </q-card-actions>
